79ac32a95f3de7b6aac33364421e944893f953aa
Daniel Rojas <38567772+DanielROJ@users.noreply.github.com>
2019-11-23 20:15:19 -0500

    Update BloodChain.sol

--- Scripts/BloodChain.sol
+++ Scripts/BloodChain.sol
@@ -12,10 +12,71 @@ event DatosCargadosExitosamente(bool);
 event EmpleadoCreadoExitosamente(uint idEmpleado);
 event LoteCreadoExitosamente(uint idLote);
 
+    //listing 3
+    event TraceExists(uint msgOrder, uint from_tru, uint to_tru);
+    event TraceDoesNotExist(uint msgOrder, uint from_tru, uint to_tru);
+    event ActivityCreated(uint msgOrder, uint activityId, address direccionLugar, uint consumedTruId, uint producedTruId);
+    event TruCreated(uint msgOrder, uint truId);
+    event TruConsumed(uint msgOrder, uint truId, uint activityId, address direccionLugar);
+    event TruProducedBy(uint msgOrder, uint currTru, uint currActivity, address direccionLugar);
+    event ActivityConsumes(uint msgOrder, uint currActivity, address direccionLugar, uint currTru);
+    
+
+    constructor () public{
+        msgOrder = 0;
+    }
+    
+    //... remaining listings 2-8
+    
+    
+    //listing 2
+    modifier nonZero(uint num) {
+        if(num == 0){
+            revert('Error el numero es zero');
+        }
+        _;
+    }
+    
+    modifier truDoesNotExist(uint id){
+        if(truLookup[id].created){
+            revert('El tru ya existe');
+        }
+        _;
+    }
+    
+    modifier truExists(uint id){
+        if(truLookup[id].created != true){
+            revert('El tru no existe');
+        }
+        _;
+    }
+    
+    modifier primitiveActivityDoesNotExist(uint id){
+        if(activityLookup[id].created){
+            revert('El activiti ya fue creado');
+        }
+        _;
+    }
+    
+    modifier primitiveActivityExists(uint id){
+        if(activityLookup[id].created != true){
+            revert();
+        }
+        _;
+    }
+    
+    modifier truAvailable(uint id){
+        if(truLookup[id].consumed || truLookup[id].used){
+            revert();
+        }
+        _;
+    }
+    
 struct BancosSangre{
     uint idBanco;
     string direccion;
     string nombre;
+    uint cantidadLotes;
     mapping(uint => Empleado) EmpleadosAsociados;
     mapping(uint => uint) listaLotesPropios;
 }
@@ -31,12 +92,24 @@ struct Empleado{
 }
 
 
-struct Ubicacion{
-    string long;
-    string lat;
-}
-
-
+  struct Tru{ // Unidad de valor, en pocas palabra que es lo que se esta firmando 
+        bool consumed;
+        bool used;
+        bool created;
+        uint id;
+        uint producedBy;
+        uint consumedBy;
+        string long;
+        string lat;
+    }
+    
+    struct PrimitiveActivity{  //Actores que pueden realizar un Trazo
+        bool created;
+        address direccionLugar;
+        uint id;
+        uint inputTruId;
+        uint outputTruId;
+    }
 
 
 struct Documento{
@@ -56,7 +129,8 @@ struct Donante{
     uint[] peso;
     uint[] altura;
     uint[] TensionArterial;
-    Donacion[] listaDonaciones;
+    uint cantidadDonaciones;
+    bool valido;
 }
 
 struct Donacion{
@@ -65,7 +139,9 @@ struct Donacion{
     uint numeroVenoponuciones;
     uint numeroMinExtraccion;
     string fechaDonacion;
-    Ubicacion ubicacion;
+    uint idDonante;
+    uint TruInicio;
+    uint TruEnd;
     Documento HashConsentimiento;
     Documento HashEiquetaUnidad;
     Documento HashFirmaDonante;
@@ -75,7 +151,8 @@ struct Donacion{
 
 struct LoteDonacion{
     uint idLote;
-    Ubicacion ubicacion;
+    Tru inicio;
+    Tru end;
     mapping(uint=> Donacion) listaUnidades;
 }
 
@@ -86,6 +163,13 @@ struct LoteDonacion{
 mapping(uint=>LoteDonacion) listaLotesTotales;
 mapping(uint=>Donante) listaDonantes;
 mapping(address=>BancosSangre) listaBancos;
+    
+mapping(uint => Tru) truLookup;
+mapping(uint => PrimitiveActivity) activityLookup; // Lista de actores que realizaron un trazo 
+    
+    uint msgOrder;
+    
+
 
 
 //Busqueda de una Donacion por lote.
@@ -102,7 +186,7 @@ function getDonacionUsuario()public {
 
 //creacion de una donacion que se agrega aun lote.
 function setDonacion(address _BancosSangre, uint idDonacion,uint _idLote, uint _cantidadExtraida, uint numeroVenoponuciones, uint numeroMinExtraccion, string memory fechaDonacion, TipoBolsa tipo, uint idMedico, uint idUser)public{
-    if(listaLotesTotales[_idLote].listaUnidades[idDonacion].idDonacion == 0){
+    if(listaLotesTotales[_idLote].listaUnidades[idDonacion].idDonacion == 0 && listaDonantes[idUser].valido == true ){
         listaLotesTotales[_idLote].listaUnidades[idDonacion].idDonacion = idDonacion;
         listaLotesTotales[_idLote].listaUnidades[idDonacion].cantidadExtraida = _cantidadExtraida;
         listaLotesTotales[_idLote].listaUnidades[idDonacion].numeroVenoponuciones = numeroVenoponuciones;
@@ -110,8 +194,9 @@ function setDonacion(address _BancosSangre, uint idDonacion,uint _idLote, uint _
         listaLotesTotales[_idLote].listaUnidades[idDonacion].fechaDonacion = fechaDonacion;
         listaLotesTotales[_idLote].listaUnidades[idDonacion].tipBolsa = tipo;
         listaLotesTotales[_idLote].listaUnidades[idDonacion].medicoEncargado = listaBancos[_BancosSangre].EmpleadosAsociados[idMedico];
-        listaDonantes[idUser].listaDonaciones.push(listaLotesTotales[_idLote].listaUnidades[idDonacion]);
-        listaBancos[msg.sender].listaLotesPropios
+        listaDonantes[idUser].cantidadDonaciones++;
+        
+        listaBancos[msg.sender].listaLotesPropios[listaBancos[msg.sender].cantidadLotes++] = _idLote;
         emit DatosCargadosExitosamente(true);
     }else{
         revert('ERROR DE CREACION');
@@ -121,7 +206,7 @@ function setDonacion(address _BancosSangre, uint idDonacion,uint _idLote, uint _
 
 
 // creacion de un donante de sangre
-function setDonante(uint cedula, uint edad, uint numeroTelefono, string memory nombre, string memory apellido, string memory Eps, string memory correoElectronico, string memory genero)public{
+function setDonante(uint cedula, uint edad, uint numeroTelefono, string memory nombre, string memory apellido, string memory Eps, string memory correoElectronico, string memory genero, bool valido)public{
 
 if(listaDonantes[cedula].cedula == 0){
     listaDonantes[cedula].cedula = cedula;
@@ -132,6 +217,7 @@ if(listaDonantes[cedula].cedula == 0){
     listaDonantes[cedula].Eps = Eps;
     listaDonantes[cedula].correoElectronico = correoElectronico;
     listaDonantes[cedula].genero = genero;
+    listaDonantes[cedula].valido = valido;
     emit DonanteCreadoExitosamente(cedula);
 }else{
     emit ErrorDeCreacion();
@@ -204,25 +290,106 @@ function getDonante(uint _cedula) public view returns(uint cedula, uint edad, ui
 }
 
 
-
-
-function setUbicacionUnidad(uint lote,  uint donacion, string  memory lat, string memory long) public{
-    listaLotesTotales[lote].listaUnidades[donacion].ubicacion = Ubicacion({long:long,lat:lat});
-}
-
-function setUbicacionLote(uint lote, string  memory lat, string memory long) public{
-    listaLotesTotales[lote].ubicacion = Ubicacion({long:long,lat:lat});
-}
-
-
 function getUbicacionUnidad(uint lote, uint donacion) public view returns(string memory, string memory){
-    return(listaLotesTotales[lote].listaUnidades[donacion].ubicacion.long, listaLotesTotales[lote].listaUnidades[donacion].ubicacion.lat);
+    uint idtru = listaLotesTotales[lote].listaUnidades[donacion].TruEnd;
+    return(truLookup[idtru].long, truLookup[idtru].lat);
 }
 
 
 
 function getUbicacionLote(uint lote) public view returns(string memory, string memory){
-    return(listaLotesTotales[lote].ubicacion.long, listaLotesTotales[lote].ubicacion.lat);
+    return(listaLotesTotales[lote].end.long, listaLotesTotales[lote].inicio.lat);
 }
+
+    //listing 4
+    function newTru(uint id) private //Funcion que permite la creacion de la unidad de valor
+    truDoesNotExist(id)
+    nonZero(id)
+    {
+        truLookup[id].created = true;
+        truLookup[id].id = id;
+        truLookup[id].consumed = false;
+        truLookup[id].used = false;
+        truLookup[id].producedBy = 0;
+        truLookup[id].consumedBy = 0;
+        emit TruCreated(msgOrder++, id);
+    }
+    
+    //listing 5
+    function newTru(uint id, uint activityId) private // Metodo que  firma la unidad de valor por su creador.
+    truDoesNotExist(id)
+    nonZero(id)
+    primitiveActivityExists(activityId)
+    {
+        newTru(id);
+        truLookup[id].producedBy = activityId;
+        
+    }
+    
+    //listing 6
+    function consumeTru(uint truId, uint activityId,string memory long, string memory lat, uint idUnidad, uint idLote) private //Meotodo que firman que la unidad de valor fue recibida.
+    truExists(truId)
+    truAvailable(truId)
+    primitiveActivityExists(activityId)
+    {
+        truLookup[truId].consumed = true;
+        truLookup[truId].long = long;
+        truLookup[truId].lat = lat;
+        truLookup[truId].consumedBy = activityId;
+        if(listaLotesTotales[idLote].listaUnidades[idUnidad].TruInicio ==0){
+            listaLotesTotales[idLote].listaUnidades[idUnidad].TruInicio = truId;
+            listaLotesTotales[idLote].listaUnidades[idUnidad].TruEnd = truId;
+        }else{
+             listaLotesTotales[idLote].listaUnidades[idUnidad].TruEnd = truId; 
+        }
+        emit TruConsumed(msgOrder++, truId, activityId, activityLookup[activityId].direccionLugar);
+        
+    }
+    
+    //listing 7
+    function newPrimitiveActivity(address name, uint idTrazo, uint inputTruId, uint outputTruId,string memory long, string memory lat, uint idUnidad, uint idLote) // Metodo que permite Crear un Autor que podra firmar el trazo
+    truAvailable(inputTruId)
+    truDoesNotExist(outputTruId)
+    primitiveActivityDoesNotExist(idTrazo)
+    nonZero(idTrazo)
+    public {
+        activityLookup[idTrazo].direccionLugar = name;
+        activityLookup[idTrazo].id = idTrazo;
+        activityLookup[idTrazo].created = true;
+        
+        if(truLookup[inputTruId].created != true){
+            newTru(inputTruId);
+        }
+        emit ActivityCreated(msgOrder++, idTrazo, name, inputTruId, outputTruId);
+        consumeTru(inputTruId, idTrazo,long, lat,idUnidad, idLote);
+        activityLookup[idTrazo].inputTruId = inputTruId;
+        newTru(outputTruId, idTrazo);
+        activityLookup[idTrazo].outputTruId = outputTruId;
+    }
+    
+    //listing 8
+    function primitiveTrace(uint tru_begin, uint tru_end) //metodo que realiza el dibujo de la Trazabilidad  segun quien lo creo y consumio.
+    truExists(tru_begin)
+    truExists(tru_end)
+    public  {
+        uint currTru = tru_begin;
+        
+        while(currTru != tru_end && truLookup[currTru].producedBy != 0){
+            uint currActivity = truLookup[currTru].producedBy;
+            address dir  = activityLookup[currActivity].direccionLugar;
+            emit TruProducedBy(msgOrder++, currTru, currActivity, dir);
+            currTru = activityLookup[currActivity].inputTruId;
+            dir = activityLookup[currActivity].direccionLugar;
+            emit ActivityConsumes(msgOrder++, currActivity, dir, currTru);
+        }
+        if(currTru == tru_end){
+           emit TraceExists(msgOrder++, tru_begin, tru_end);
+        }
+        else
+        {
+           emit   TraceDoesNotExist(msgOrder++,tru_begin, tru_end);
+        }
+    }
+    
 
 }
